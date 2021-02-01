39b0203723af8702a664c07df6621b51d083ca9e
zane <zane.a.karl@gmail.com>
2021-02-01 13:17:51 -0800

    Print Hex value	to screen
    Added an empty span with an id to accomplish this.

--- JS/random-background-hexcolor-button/app.js
+++ JS/random-background-hexcolor-button/app.js
@@ -1,11 +1,14 @@
 var button = document.querySelector("button");
 var body = document.querySelector("body");
+var hex_value = document.getElementById("hexvalue");
 
 body.style.backgroundColor = "gray";
-var total_hex = 16*16;
+var total_hex = 16*16-1;
 button.onclick = function() {
-    body.style.backgroundColor = "#" +
-	(Math.random()*total_hex).toString(16) +
-	(Math.random()*total_hex).toString(16) +
-	(Math.random()*total_hex).toString(16);
+    let hex = "#" +
+	Math.floor(Math.random()*total_hex).toString(16) +
+	Math.floor(Math.random()*total_hex).toString(16) +
+	Math.floor(Math.random()*total_hex).toString(16);
+    body.style.backgroundColor = hex;
+    hex_value.textContent = hex;
 }

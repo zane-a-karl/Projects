e9fe15332199440dba75760e2e5b206810712c40
zane <zane.a.karl@gmail.com>
2021-06-26 14:19:43 -0700

    Finish minimal working TicTacToe game in Bash

---
+++ Bash/Games/test.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+read -rsn1 input
+if [ "$input" = "a" ]; then
+    echo "hello world"
+fi
+done
--- Bash/RyansTutorials/Games/TicTacToe.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#  This is an implementation of the game Tic-Tac-Toe
-# it's rules and assumptions are as follows:
-#   - We will print a game board
-#   - We will have 1 player and 2 player modes

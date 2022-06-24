8baf161a955bea3b049907a9fbbdfdc72997a2d0
zane <zane.a.karl@gmail.com>
2022-06-24 11:47:56 -0700

    Start blind coding of guessing game
    Blind just means that you aren't looking at the book's
    instructions but writting the program on your own. You tried to
    get the StdRng tuple struct working but the compiler kept
    complaining about not being able to create it due to the StdRng
    struct having private fields... which it doesn't have. You'll
    have to look into this more in the future you only spent 15 min
    on it.

---
+++ Rust/Rust-Books/the-book-blind/guessing_game/src/main.rs
@@ -0,0 +1,14 @@
+///  Here’s how it works: the program will generate a random
+/// integer between 1 and 100. It will then prompt the player
+/// to enter a guess. After a guess is entered, the program
+/// will indicate whether the guess is too low or too high.
+/// If the guess is correct, the game will print a
+/// congratulatory message and exit.
+use rand::rngs::StdRng;
+use rand::Rng;
+
+fn main() {
+    let mut stdrng = StdRng();
+    let rn: u32 = stdrng.gen();
+    println!("{rn:?}");
+}

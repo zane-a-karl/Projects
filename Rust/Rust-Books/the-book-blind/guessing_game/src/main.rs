407653549a6a4b49f339027eeefcfbd4479fa827
zane <zane.a.karl@gmail.com>
2022-07-26 21:41:57 -0700

    Complete the beginning of the rust book without looking at the book
    The idea is to see if you can use your knowledge of rust and access
    to the documentation to figure out how to do all of the same exercises
    that the book asks you to complete. You got all of these fairly quickly!

--- Rust/Rust-Books/the-book-blind/guessing_game/src/main.rs
+++ Rust/Rust-Books/the-book-blind/guessing_game/src/main.rs
@@ -4,22 +4,48 @@
 /// will indicate whether the guess is too low or too high.
 /// If the guess is correct, the game will print a
 /// congratulatory message and exit.
-use rand::prelude::*;
-use std::io::stdin;
+use rand::{rngs::ThreadRng, Rng};
+use std::cmp::Ordering;
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     let mut thread_rng = ThreadRng::default();
-    let mut rn: i32 = thread_rng.gen::<i32>() % 99 + 1;
-    println!("{rn}");
-    print!("Please enter a guess: ");
-    let mut guess_buf = String::new();
-    if let Ok(n) = stdin().read_line(&mut guess_buf) {
-        print!("{guess_buf}");
-        println!("read {n} bytes");
-        println!(
-            "you were off by {}!",
-            rn - guess_buf.trim().parse::<i32>().unwrap()
-        );
-        println!("{rn}");
+    println!("Welcome to the guessing game!");
+    println!("Guess the random number between 1 and 100 in as few guesses as possible!");
+    let rn: u32 = ((thread_rng.gen::<u32>()) % 99) + 1;
+    //    println!("The random number is = {rn}");
+		// Note: this is how you label a loop in Rust
+		// you can also label your `break` and `continue` stmts
+		// This lets you break out of an outer loop from within
+		// an inner loop! If you don't specify loop labels then
+		// breaking and continuing only applies to the loop you
+		// are breaking or continuing from!
+    'main_event_loop: loop {
+        print!("Enter a guess: ");
+        stdout().flush().unwrap(); // Needed to get the print order correct
+        let mut guess_buf = String::new();
+        if let Ok(_n_bytes_read) = stdin().read_line(&mut guess_buf) {
+            let guess_as_u32 = match guess_buf.trim().parse::<u32>() {
+                Ok(n) => {
+                    if n < 1 || n > 100 {
+                        println!("Integers must be between 1 and 100.");
+                        continue 'main_event_loop;
+                    }
+                    n
+                }
+                Err(_) => {
+                    println!("Only integers are allowed.");
+                    continue 'main_event_loop;
+                }
+            };
+            match guess_as_u32.cmp(&rn) {
+                Ordering::Greater => println!("Too High!"),
+                Ordering::Less => println!("Too Low!"),
+                Ordering::Equal => {
+                    println!("Spot on!");
+                    break 'main_event_loop;
+                }
+            };
+        }
     }
 }
